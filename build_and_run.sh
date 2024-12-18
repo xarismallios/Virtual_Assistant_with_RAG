f47817f426e1c58d4c67a5419c21ba04e9f91d90
Xaris Mallios <61496682+xarismallios@users.noreply.github.com>
2024-12-18 23:08:58 +0200

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
 
-# Build the Docker image
-docker build -t flask-app .
-
-# Run the Docker container
-docker run -d -p 8000:8000 flask-app
+# Build the Docker image. Run this inside project folder.
+docker-compose up
